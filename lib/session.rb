2c4d8fdf07390817aef703e4495ecb81115174b9
Walid Arghandewal <walid.argh@gmail.com>
2016-04-06 17:11:54 -0700

    Phase 6 done

---
+++ lib/session.rb
@@ -0,0 +1,29 @@
+require 'json'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  def initialize(req)
+    cookie = req.cookies['_rails_lite_app']
+    @cookie_val = {}
+    if cookie
+      @cookie_val = JSON.parse(cookie)
+    end
+  end
+
+  def [](key)
+    @cookie_val[key]
+  end
+
+  def []=(key, val)
+    @cookie_val[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    p @cookie_val
+    res.set_cookie('_rails_lite_app', {path: '/', value: @cookie_val.to_json})
+    res.finish
+  end
+end
